ee81fe0969329bf5c984b0765f9b2b893a06c137
seize <seize@SEIZE-LG>
2018-01-31 16:17:29 +0900

    init

--- KBS-CONTENTS-CIRCULATION/src/main/java/kr/co/kbs/distribute/common/controller/MDLController.java
+++
@@ -1,8 +0,0 @@
-package kr.co.kbs.distribute.common.controller;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public interface MDLController {
-	public static final Logger log = LoggerFactory.getLogger(Runnable.class.getName());
-}
--- KBS-CONTENTS-CIRCULATION/src/main/java/kr/co/kbs/distribute/common/service/CommonService.java
+++
@@ -1,8 +0,0 @@
-package kr.co.kbs.distribute.common.service;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public interface CommonService {
-	public static final Logger log = LoggerFactory.getLogger(Runnable.class.getName());
-}
